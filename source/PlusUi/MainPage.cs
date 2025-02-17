5b346e15f32a0f74ef6c856fe3afb6e912c2fd12
Bernhard Pollerspöck <bernhard@pollerspoeck.net>
2025-02-17 20:09:30 +0100

    #10

--- source/PlusUi/MainPage.cs
+++ source/PlusUi/MainPage.cs
@@ -21,27 +21,35 @@ public class MainPage(MainViewModel vm) : UiPageElement(vm)
                 vm.Count++;
             }
         });
-        return new VStack(
-            new Solid(height: 10, color: SKColors.Green)
-                .BindDesiredWidth(nameof(vm.Count), () => vm.Count * 3)
-                .SetMargin(new(0, 10, 0, 0)),
-            new Solid(50, 10, SKColors.Red),
-            new VStack(
-                new Solid(10, 10, SKColors.Blue),
-                new Solid(10, 10, SKColors.Purple))
-                .SetHorizontalAlignment(HorizontalAlignment.Right)
-                .SetMargin(new(10, 0, 0, 0)),
-            new Label()
-                .SetText("Hello World !"),
-            new Button()
-                .SetCommand(new SyncCommand(() => vm.Count = 0))
-                .SetText("Click me")
-                .SetMargin(new(10, 0, 0, 0)),
-            new Label()
-                .BindText(nameof(vm.Count), () => vm.Count.ToString())
-                .SetTextColor(SKColors.Black)
-                .SetBackgroundColor(SKColors.Aqua))
-            .SetMargin(new(10, 0, 0, 0));
+
+        return new Label()
+            .SetText("Hello World !")
+            .SetTextSize(50)
+            .SetBackgroundColor(SKColors.Aqua)
+            .SetHorizontalAlignment(HorizontalAlignment.Center)
+            .SetVerticalAlignment(VerticalAlignment.Center);
+
+        //return new VStack(
+        //    new Solid(height: 10, color: SKColors.Green)
+        //        .BindDesiredWidth(nameof(vm.Count), () => vm.Count * 3)
+        //        .SetMargin(new(0, 10, 0, 0)),
+        //    new Solid(50, 10, SKColors.Red),
+        //    new VStack(
+        //        new Solid(10, 10, SKColors.Blue),
+        //        new Solid(10, 10, SKColors.Purple))
+        //        .SetHorizontalAlignment(HorizontalAlignment.Right)
+        //        .SetMargin(new(10, 0, 0, 0)),
+        //    new Label()
+        //        .SetText("Hello World !"),
+        //    new Button()
+        //        .SetCommand(new SyncCommand(() => vm.Count = 0))
+        //        .SetText("Click me")
+        //        .SetMargin(new(10, 0, 0, 0)),
+        //    new Label()
+        //        .BindText(nameof(vm.Count), () => vm.Count.ToString())
+        //        .SetTextColor(SKColors.Black)
+        //        .SetBackgroundColor(SKColors.Aqua))
+        //    .SetMargin(new(10, 0, 0, 0));
     }
 
 
