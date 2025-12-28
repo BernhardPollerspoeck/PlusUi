7415061269c5d28813ccb70a56eb83e1392e7c34
Bernhard Pollerspöck <office@qsp.app>
2025-12-28 19:59:12 +0100

    cleanup

--- tests/PlusUi.Regression.Application/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-
-using PlusUi.Headless;
-using PlusUi.Headless.Enumerations;
-
-using var headless = PlusUiHeadless.Create(new App(), ImageFormat.Png);
-
-var frame = await headless.GetCurrentFrameAsync();
-await File.WriteAllBytesAsync("001.png", frame);
